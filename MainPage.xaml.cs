f570493d08c870a72ed685e2e9139aa48933daef
RainbowWolfer <RainbowWolfer@outlook.com>
2020-12-27 16:36:03 +0800

    Finish A* Pathfinding

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -108,8 +108,16 @@ namespace MazeTest {
 			await Task.Delay(10);
 			finishGenerating = true;
 			var pf = new Pathfinding(nodes);
-			pf.Log();
-			pf.DrawInGrid();
+			//pf.Log();
+			List<PFNode> path = pf.FindPath(new Vector2(0, 0), new Vector2(row * 2 - 2, col * 2 - 2));
+			List<Node> convertedPath = pf.ConvertToOriginalPath(path);
+			pf.DrawInGrid(convertedPath);
+
+			foreach(Node item in nodes) {
+				if(convertedPath.Contains(item)) {
+					grids.Find((g) => g.node == item).SetFill(Colors.Green);
+				}
+			}
 		}
 
 		public Node BackTrace(Node node) {
@@ -207,31 +215,30 @@ namespace MazeTest {
 		}
 	}
 	public class Pathfinding {
-		private Node[] originNodes;
+		private readonly List<Node> originalNode;
+		public const int MOVE_DIAGONAL_COST = 14;
+		public const int MOVE_STRAIGHT_COST = 10;
 
 		private int row => MainPage.Instance.row * 2 - 1;
 		private int col => MainPage.Instance.col * 2 - 1;
 
-		public Dictionary<Vector2, Node> nodes;
+		public Dictionary<Vector2, PFNode> nodes;
 		public Pathfinding(List<Node> ns) {
-			originNodes = ns.ToArray();
-			nodes = new Dictionary<Vector2, Node>();
-
+			originalNode = ns;
+			nodes = new Dictionary<Vector2, PFNode>();
 			foreach(Node n in ns) {
-				//nodes.Add(n.pos, n);
-				n.walkable = true;
-				nodes[n.pos * 2] = n;
+				nodes[n.pos * 2] = new PFNode(n.pos * 2) { walkable = true };
 				for(int i = 0; i < 4; i++) {
 					Vector2 v = n.pos * 2 + Vector2.AllDirections[i];
 					if(IsWithinBorder(v)) {
 						if(i == 0 && n.d.left) {
-							nodes[v] = new Node(v) { walkable = true };
+							nodes[v] = new PFNode(v) { walkable = true };
 						} else if(i == 1 && n.d.up) {
-							nodes[v] = new Node(v) { walkable = true };
+							nodes[v] = new PFNode(v) { walkable = true };
 						} else if(i == 2 && n.d.right) {
-							nodes[v] = new Node(v) { walkable = true };
+							nodes[v] = new PFNode(v) { walkable = true };
 						} else if(i == 3 && n.d.down) {
-							nodes[v] = new Node(v) { walkable = true };
+							nodes[v] = new PFNode(v) { walkable = true };
 						}
 					}
 				}
@@ -239,15 +246,124 @@ namespace MazeTest {
 			for(int x = 0; x < row; x++) {
 				for(int y = 0; y < col; y++) {
 					if(!nodes.ContainsKey(new Vector2(x, y))) {
-						nodes.Add(new Vector2(x, y), new Node(x, y) { walkable = false });
+						nodes.Add(new Vector2(x, y), new PFNode(x, y) { walkable = false });
 					}
 				}
 			}
 		}
-		public List<Node> FindPath(Vector2 start, Vector2 end) {
 
-			return new List<Node>();
+		private static List<PFNode> openList;
+		private static List<PFNode> closeList;
+
+		public List<Node> ConvertToOriginalPath(List<PFNode> nodes) {
+			List<Node> result = new List<Node>();
+			foreach(PFNode pf in nodes) {
+				foreach(Node n in originalNode) {
+					if(pf.pos / 2 == n.pos) {
+						if(pf.pos.x % 2 != 1 && pf.pos.y % 2 != 1) {
+							result.Add(n);
+						}
+						break;
+					}
+				}
+			}
+			return result;
 		}
+
+		public List<PFNode> FindPath(Vector2 start, Vector2 end) {
+			PFNode startNode = nodes[start];
+			PFNode endNode = nodes[end];
+
+			openList = new List<PFNode>() { startNode };
+			closeList = new List<PFNode>();
+
+			foreach(PFNode node in nodes.Values) {
+				if(!node.walkable) {
+					closeList.Add(node);
+				}
+			}
+
+			for(int x = 0; x < row; x++) {
+				for(int y = 0; y < col; y++) {
+					PFNode node = nodes[new Vector2(x, y)];
+					node.gCost = int.MaxValue;
+					node.previous = null;
+				}
+			}
+
+			startNode.gCost = 0;
+			startNode.hCost = CalculateDistanceCost(startNode, endNode);
+
+			while(openList.Count > 0) {
+				PFNode currentNode = GetLowestFCostNode(openList);
+				if(currentNode == endNode) {
+					return CalculatePath(endNode);
+				}
+				openList.Remove(currentNode);
+				closeList.Add(currentNode);
+
+				List<PFNode> ns = GetNeighbours(currentNode);
+				foreach(PFNode node in ns) {
+					if(closeList.Contains(node)) {
+						continue;
+					}
+					int distanceCost = CalculateDistanceCost(currentNode, node);
+					int tentativeGCost = currentNode.gCost + distanceCost;
+					if(tentativeGCost < node.gCost) {
+						node.previous = currentNode;
+						node.gCost = tentativeGCost;
+						node.hCost = CalculateDistanceCost(node, endNode);
+
+						if(!openList.Contains(node)) {
+							openList.Add(node);
+						}
+					}
+				}
+			}
+			throw new Exception("No Path Found");
+		}
+		public List<PFNode> GetNeighbours(PFNode current) {
+			List<PFNode> result = new List<PFNode>();
+			if(current.pos.x >= 1) {
+				result.Add(nodes[current.pos + Vector2.Left]);
+			}
+			if(current.pos.x < row - 1) {
+				result.Add(nodes[current.pos + Vector2.Right]);
+			}
+			if(current.pos.y >= 1) {
+				result.Add(nodes[current.pos + Vector2.Up]);
+			}
+			if(current.pos.y < col - 1) {
+				result.Add(nodes[current.pos + Vector2.Down]);
+			}
+			return result;
+		}
+		public List<PFNode> CalculatePath(PFNode endNode) {
+			List<PFNode> path = new List<PFNode>() { endNode };
+			PFNode current = endNode;
+			while(current.previous != null) {
+				path.Add(current.previous);
+				current = current.previous;
+			}
+			path.Reverse();
+			return path;
+		}
+		public PFNode GetLowestFCostNode(List<PFNode> ns) {
+			PFNode lowest = ns[0];
+			foreach(PFNode item in ns) {
+				if(item.fCost < lowest.fCost) {
+					lowest = item;
+				}
+			}
+			return lowest;
+		}
+		public int CalculateDistanceCost(PFNode a, PFNode b) {
+			int xDistance = Math.Abs(a.pos.x - b.pos.y);
+			int yDistance = Math.Abs(a.pos.y - b.pos.y);
+			int remaining = Math.Abs(xDistance - yDistance);
+			return MOVE_DIAGONAL_COST * Math.Min(xDistance, yDistance) + MOVE_STRAIGHT_COST * remaining;
+		}
+
 		public bool IsWithinBorder(Vector2 v) {
 			return v.x >= 0 && v.y >= 0 && v.x < row && v.y < col;
 		}
@@ -262,20 +378,26 @@ namespace MazeTest {
 				Debug.WriteLine(line + "\n");
 			}
 		}
-		public void DrawInGrid() {
+		public void DrawInGrid(List<Node> path) {
 			for(int x = 0; x < row; x++) {
 				MainPage._testGrid.RowDefinitions.Add(new RowDefinition());
 			}
 			for(int y = 0; y < col; y++) {
 				MainPage._testGrid.ColumnDefinitions.Add(new ColumnDefinition());
 			}
-
 			for(int x = 0; x < row; x++) {
 				for(int y = 0; y < col; y++) {
 					string str = x + ":" + y + "\n";
 					str += nodes[new Vector2(x, y)].walkable ? "YES\n" : "no\n";
-					SingleGrid sg = new SingleGrid(nodes[new Vector2(x, y)], str);
-					sg.SetFill(nodes[new Vector2(x, y)].walkable ? Colors.Transparent : Colors.Red);
+					SingleGrid sg = new SingleGrid(null, str);
+					bool inPath = false;
+					foreach(Node n in path) {
+						if(n.pos * 2 == new Vector2(x, y)) {
+							inPath = true;
+							break;
+						}
+					}
+					sg.SetFill(nodes[new Vector2(x, y)].walkable ? inPath ? Colors.Blue : Colors.Transparent : Colors.Red);
 					MainPage._testGrid.Children.Add(sg);
 					Grid.SetRow(sg, y);
 					Grid.SetColumn(sg, x);
@@ -283,17 +405,26 @@ namespace MazeTest {
 			}
 		}
 	}
+	public class PFNode {
+		public Vector2 pos;
+		public bool walkable;
+		public int gCost;
+		public int hCost;
+		public int fCost => gCost + hCost;
+		public PFNode previous;
+		public PFNode(Vector2 pos) {
+			this.pos = pos;
+		}
+		public PFNode(int x, int y) {
+			this.pos = new Vector2(x, y);
+		}
+	}
+
 	public class Node {
 		public Vector2 pos;
 		public Direction d;
 
 		public bool onWay;
-
-		public bool walkable;
-
-		public int gCost;
-		public int hCost;
-		public int fCost => gCost + hCost;
 
 		public Node previous;
 		public Node(int x, int y) {
@@ -347,6 +478,9 @@ namespace MazeTest {
 		}
 		public static Vector2 operator *(Vector2 a, int b) {
 			return new Vector2(a.x * b, a.y * b);
+		}
+		public static Vector2 operator /(Vector2 a, int b) {
+			return new Vector2(a.x / b, a.y / b);
 		}
 		public static bool operator ==(Vector2 a, Vector2 b) {
 			return a.x == b.x && a.y == b.y;
